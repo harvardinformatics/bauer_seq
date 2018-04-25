d883b3d55ac77fa711f7a5581c00570b035c2bfa
portermahoney <portermahoney@gmail.com>
2018-04-25 17:59:23 -0400

    more run api

--- sequencing/urls.py
+++ sequencing/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import Create
+from .views import RunCreate, RunMod
 
 urlpatterns = {
-        url(r'^runs/$', Create.as_view(), name='create')
+        url(r'^runs/$', RunCreate.as_view(), name='run create'),
+        url(r'^runs/(?P<pk>[0-9]+)/$', RunMod.as_view(), name='run modify')
 }
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- sequencing/views.py
+++ sequencing/views.py
@@ -3,9 +3,13 @@ from rest_framework import generics
 from .serializers import RunSerializer
 from .models import Run
 
-class Create(generics.ListCreateAPIView):
+class RunCreate(generics.ListCreateAPIView):
     queryset = Run.objects.all()
     serializer_class = RunSerializer
 
     def perform_create(self, serializer):
         serializer.save()
+
+class RunMod(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Run.objects.all()
+    serializer_class = RunSerializer
