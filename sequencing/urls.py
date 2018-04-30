74fbf7c5a511fabb6660a3433ab49fcc3f46f427
Aaron Kitzmiller <aaron_kitzmiller@harvard.edu>
2018-04-30 15:02:15 -0400

    added nginx/supervisor to docker production; added dockerfile-dev; moved wsgi.py file to root of project so that nginx/gunicorn/supervisor works the same way as others; moved sequencing test into test dir; moved pip installs to a requirements file; added some Instrument API components

--- sequencing/urls.py
+++ sequencing/urls.py
@@ -3,6 +3,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from .views import *
 
 urlpatterns = {
+        url(r'^instruments/$', InstrumentCreate.as_view(), name='instrument create'),
         url(r'^runs/$', RunCreate.as_view(), name='run create'),
         url(r'^runs/(?P<pk>[0-9]+)/$', RunMod.as_view(), name='run modify'),
         url(r'^lanes/$', LaneCreate.as_view(), name='lane create'),
