febae580822cc5ce93018c43393970dde1fe6633
portermahoney <portermahoney@gmail.com>
2018-05-08 13:34:52 -0400

    add debug and allowed hosts

--- bauer/settings.py
+++ bauer/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['BAUER_DJANGO_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('BAUER_DJANGO_DEBUG', 'FALSE') == 'TRUE')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
