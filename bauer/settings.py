7dcbd966ec4874e913262df6320990b64cef278e
Aaron Kitzmiller <aaron_kitzmiller@harvard.edu>
2018-05-08 14:08:06 -0400

    add STATIC_ROOT, STATIC_URL for collectstatic; modified nginx static

--- bauer/settings.py
+++ bauer/settings.py
@@ -128,7 +128,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/bauer/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 REST_FRAMEWORK = {
         'DEFAULT_AUTHENTICATION_CLASSES': (
